75006f802a56e0f39861cd5c9445ea3ffe0ad832
Mati Sediqi <m.sediqi@schommer-media.de>
2021-02-05 21:58:53 +0100

    Init commit

---
+++ Sync.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Iammati\Synchro;
+
+use Phar;
+use PharData;
+use Symfony\Component\Yaml\Yaml;
+
+class Sync
+{
+    /**
+     * @var array
+     */
+    protected $ddevConfig;
+
+    /**
+     * @param string $ddevDir
+     */
+    public function __construct()
+    {
+        $this->ddevConfig = Yaml::parseFile($_SERVER['DOCUMENT_ROOT'] . '/.ddev/config.yaml');
+    }
+
+    /**
+     * @param string $source
+     */
+    public function create(string $source): self
+    {
+        $projectName = $this->ddevConfig['name'];
+        $dateTimeStamp = date('Y-m-d') . '_' . date('H-i');
+
+        $name = $projectName . '_' . $dateTimeStamp;
+        echo "Creating new $name.tar.gz...\n";
+
+        try {
+            $pharData = new PharData($name . '.tar');
+
+            $pharData->buildFromDirectory($source);
+            $pharData->compress(Phar::GZ);
+
+            unlink($name . '.tar');
+
+            echo "Done.\n\n";
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        return $this;
+    }
+}
