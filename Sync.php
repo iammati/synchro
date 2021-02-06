5ef1e0bcec8a530c82f1181d0e5fe438f5060dc4
Mati Sediqi <m.sediqi@schommer-media.de>
2021-02-06 22:36:34 +0100

    [UPDATE] Changed synchro.php into a bin-exec-file to make it runnable by ddev's crontab.

--- Sync.php
+++ Sync.php
@@ -2,14 +2,7 @@
 
 namespace Iammati\Synchro;
 
-use CallbackFilterIterator;
-use Phar;
-use PharData;
-use PharException;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
 use Symfony\Component\Yaml\Yaml;
-use UnexpectedValueException;
 
 class Sync
 {
@@ -26,22 +19,18 @@ class Sync
     /**
      * @param string $ddevDir
      */
-    public function __construct(string $host, string $ddev = '')
+    public function __construct(string $host, string $ddev)
     {
-        if ($ddev == '') {
-            //
-            $ddev = $_SERVER['DOCUMENT_ROOT'] . '/.ddev';
-        }
-
         $this->ddevConfig = Yaml::parseFile($ddev . '/config.yaml');
 
         $this->host = $host;
     }
 
     /**
-     * @param array|string $sources
+     * @param array $excludes
+     * @param mixed $sources
      */
-    public function create(...$sources): self
+    public function create(array $excludes, ...$sources): self
     {
         $start = microtime(true);
 
@@ -50,10 +39,14 @@ class Sync
 
         $name = $projectName . '_' . $dateTimeStamp;
 
+        $excludesStr = '';
+
+        foreach ($excludes as $exclude) {
+            $excludesStr .= ' --exclude ' . $exclude;
+        }
+
         foreach ($sources as $source) {
             echo "\nCreating new '$name.tar.gz' for source '$source'...\n";
-
-            $path = realpath($source);
 
             $sourceName = $name . '_' . $source;
 
@@ -61,34 +54,19 @@ class Sync
             $sourceName = str_replace('.', '', $sourceName);
             $sourceName = str_replace('/', '-', $sourceName);
 
-            // $iterator = new RecursiveIteratorIterator(
-            //     new RecursiveDirectoryIterator($path)
-            // );
-
-            // $filterIterator = new CallbackFilterIterator($iterator, function ($file) {
-            //     return (
-            //         strpos($file, 'vendor/') === false &&
-            //         strpos($file, 'node_modules/') === false
-            //     );
-            // });
-
             try {
-                $pharData = new PharData($sourceName . '.tar');
+                exec("tar -zcvf $sourceName.tar.gz$excludesStr $source /dev/null 2>&1");
 
-                $pharData->buildFromDirectory('./../Configuration');
-                $pharData->compress(Phar::GZ);
-
-                unlink($sourceName . '.tar');
-
-                exec("ssh dev@{$this->host} 'mkdir /home/dev/ddev-projects/$projectName' </dev/null 2>&1");
+                exec("ssh dev@{$this->host} 'mkdir /home/dev/ddev-projects/$projectName' && rm -rf /home/dev/ddev-projects/$projectName/*.tar.gz /dev/null 2>&1");
                 exec("rsync -av *.tar.gz dev@{$this->host}:/home/dev/ddev-projects/$projectName/");
 
-                unlink($sourceName . '.tar.gz');
+                unlink("$sourceName.tar.gz");
             } catch (\Exception $e) {
                 throw $e;
             } finally {
                 $timeElapsedSecs = microtime(true) - $start;
-                echo "Done after " . \round($timeElapsedSecs, 2) . " seconds.\n";
+                echo "Done after " . round($timeElapsedSecs, 2) . " seconds.\n";
+                $start = microtime(true);
             }
         }
 
--- synco.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__.'/vendor/autoload.php';
-
-$remoteHost = '192.168.1.117';
-$ddevPath = realpath(__DIR__.'/../.ddev');
-
-(new \Iammati\Synchro\Sync($remoteHost, $ddevPath))->create('../web', '../new_Build');
